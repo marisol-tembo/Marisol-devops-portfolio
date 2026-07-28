9b8b4fc59dc8f614d1306a7e03e5ded67e2e45ea
Marisol Ucan Gongora <marisol@Marisols-MacBook-Pro.local>
2026-07-27 21:33:16 -0600

    Test Terraform pull request checks

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "website" {
   bucket = "sol-tembos-bucket"
 
   tags = {
-    Project   = "marisol-devops-portfolio"
+    Name      = "marisol-devops-portfolio"
     ManagedBy = "Terraform"
   }
 }
