24d9726ab29d77c01cbf491f3fe587e0291f6404
Marisol Ucan Gongora <marisol@Marisols-MacBook-Pro.local>
2026-07-29 11:57:16 -0600

    configuring cloud front invalidation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "cloudfront_url" {
   description = "HTTPS URL for the portfolio website"
   value       = "https://${aws_cloudfront_distribution.website.domain_name}"
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.website.id
+}
